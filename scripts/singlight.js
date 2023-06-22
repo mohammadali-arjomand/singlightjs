61d8bf14d22f59a68e564e831f4c0c4097c7ba87
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-06-22 12:33:32 +0330

    improve lifecycle

--- scripts/singlight.js
+++ scripts/singlight.js
@@ -1,97 +1,77 @@
 class Singlight {
     constructor(property) {
+        // setting property
         this.property = property;
     }
 
     mount(selector) {
+        console.log("BOOTSTRAPER RUNNED");
+        // select element by selector
         this.selector = document.querySelector(selector);
+
+        // check selector is valid
         if (this.selector === null) {
             console.log("You should pass a valid selector");
         }
+
+        // check root is valid
         if (this.property.root === undefined) {
             console.log("You should pass root property");
         }
+
+        // check home is valid
         if (this.property.home === undefined) {
             console.log("You should pass home property");
         }
 
-        
-        this.current = this.property.root + "/../" + this.property.home;
-        this.history = [this.current];
-        this.render();  
+        // make current location
+        this.current = this.property.root + "/" + this.property.home;
+
+        // make history
+        this.history = [];
+
+        // bootstrapping lifecycle
+        this.urlBuilder(this.current, false);
     }
 
-    scanner() {
-        this.links();
-        this.forms();
+    urlBuilder(to) {
+        console.log("URL BUILDER RUNNED");
+        console.log(this.history);
+
+        // update current url
+        this.current = to;
+
+        // add to history
+        this.history.push(this.current);
+
+        // initial page loader
+        this.loader();
     }
 
-    links() {
-        let links = document.querySelectorAll("a");
-        links.forEach((link) => {
-            link.addEventListener("click", e => this.routingByLink(e, link.getAttribute("href")));
-        });
-    }
-
-    routingByLink(event, url) {
-        event.preventDefault();
-        this.redirect(url);
-    }
-
-    forms() {
-        let forms = document.querySelectorAll("form");
-        forms.forEach((form) => {
-            form.addEventListener("submit", e => this.routingByForm(e, form));
-        });
-    }
-
-    routingByForm(event, form) {
-        event.preventDefault();
-        let data = new FormData(form);
-
-        if (form.action !== undefined) {
-            this.switcher(this.url(form.action));
-            this.send(form.action, data);
-        }
-        else {
-            console.log("action is require");
-        }
-    }
-
-    redirect(url) {
-        this.switcher(this.url(url));
-        this.render();
-    }
-
-    send(to, data) {
-        async function submit(to, data, parent) {
+    loader() {
+        console.log("LOADER RUNNED");
+        // make request async function
+        async function request(url, self) {
             try {
-                let response = await fetch(to, {
+                // create params
+                let params = undefined;
+
+                // check params
+                if (self.params !== undefined) {
+                    params = {
                         method: "POST",
-                        body: data
-                    });
-                let text = await response.text();
-                if (response.ok) {
-                    parent.innerHTML = text;
+                        body: self.params
+                    };
                 }
-                else {
-                    console.log("Request failed");
-                }
-            }
-            catch {
-                console.log("Request failed");
-            }
-        }
-        submit(to, data, this.selector).then(e => this.scanner());
-    }
 
-    render() {
-        async function request(url, parent) {
-            try {
-                let response = await fetch(url);
+                // request to url and get page
+                let response = await fetch(url, params);
                 let text = await response.text();
+
+                // check status
                 if (response.ok) {
-                    parent.innerHTML = text;
+                    // initial page renderer
+                    self.renderer(text);
                 }
                 else {
                     console.log("Request failed");
@@ -101,17 +81,71 @@ class Singlight {
                 console.log("Request failed2");
             }
         }
-        request(this.current, this.selector).then(e => this.scanner());
+
+        // execute request async function
+        request(this.current, this);
     }
 
-    url(to) {
-        return this.current + "/../" + to;
+    renderer(page) {
+        console.log("RENDERER RUNNED");
+        // rendering page
+        this.selector.innerHTML = page;
+
+        // initial page scanner
+        this.scanner();
     }
 
-    switcher(newURL) {
-        this.current = newURL;
-        this.history.push(this.current);
-        console.log(this.history);
+    scanner() {
+        console.log("SCANNER RUNNED");
+        // scan links
+        let links = document.querySelectorAll("a");
+        links.forEach((link) => {
+            link.addEventListener("click", e => this.eventHandler(e, link));
+        });
+
+        // scan forms
+        let forms = document.querySelectorAll("form");
+        forms.forEach((form) => {
+            form.addEventListener("submit", e => this.eventHandler(e, form));
+        });
+    }
+
+    eventHandler(event, target) {
+        console.log("EVENT HANDLER RUNNED");
+        // check target tag
+        if (event.target.nodeName == "A") {
+            // stop refresh
+            event.preventDefault();
+
+            // check href is exists
+            if (target.href !== undefined) {
+                // update params to undefined
+                this.params = undefined;
+                
+                // initial url builder
+                this.urlBuilder(target.href, false);
+            }
+            else {
+                // report error
+                console.log("href Attribute is require");
+            }
+        }
+        else {
+            // stop refresh
+            event.preventDefault();
+
+            // check action is exists
+            if (target.action !== undefined) {
+                // update params to form data object
+                this.params = new FormData(target);
+
+                // initial url builder
+                this.urlBuilder(target.action, false)
+            }
+            else {
+                // report error
+                console.log("action Attribute is require");
+            }
+        }
     }
 }
-    
