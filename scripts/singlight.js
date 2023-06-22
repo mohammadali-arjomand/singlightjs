bce2618ec91aa65ab0b03bac27e3efa7d627b75d
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-06-22 10:07:52 +0330

    support forms and replace a to redirector tag

--- scripts/singlight.js
+++ scripts/singlight.js
@@ -19,16 +19,69 @@ class Singlight {
         this.render();  
     }
 
-    redirectors() {
-        let redirectors = document.querySelectorAll("redirector");
-        redirectors.forEach((redirector) => {
-            redirector.addEventListener("click", e => this.redirect(e.target.getAttribute("to")));
+    scanner() {
+        this.links();
+        this.forms();
+    }
+
+    links() {
+        let links = document.querySelectorAll("a");
+        links.forEach((link) => {
+            link.addEventListener("click", e => this.routingByLink(e, link.getAttribute("href")));
         });
+    }
+
+    routingByLink(event, url) {
+        event.preventDefault();
+        this.redirect(url);
+    }
+
+    forms() {
+        let forms = document.querySelectorAll("form");
+        forms.forEach((form) => {
+            form.addEventListener("submit", e => this.routingByForm(e, form));
+        });
+    }
+
+    routingByForm(event, form) {
+        event.preventDefault();
+        let data = new FormData(form);
+
+        if (form.action !== undefined) {
+            this.current = this.current + "/../" + form.action;
+            this.send(form.action, data);
+        }
+        else {
+            console.log("action is require");
+        }
     }
 
     redirect(url) {
         this.current = this.current + "/../" + url;
         this.render();
+    }
+
+    send(to, data) {
+        console.log(to);
+        async function submit(to, data, parent) {
+            try {
+                let response = await fetch(to, {
+                        method: "POST",
+                        body: data
+                    });
+                let text = await response.text();
+                if (response.ok) {
+                    parent.innerHTML = text;
+                }
+                else {
+                    console.log("Request failed");
+                }
+            }
+            catch {
+                console.log("Request failed");
+            }
+        }
+        submit(to, data, this.selector).then(e => this.scanner());
     }
 
     render() {
@@ -47,8 +100,7 @@ class Singlight {
                 console.log("Request failed2");
             }
         }
-        request(this.current, this.selector).then(e => this.redirectors());
-        
+        request(this.current, this.selector).then(e => this.scanner());
     }
 }
     
